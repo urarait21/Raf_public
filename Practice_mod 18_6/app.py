f96d2cb371f6c0edaffe4dcdf3887a5dd3344871
Rafit <urarafit@gmail.com>
2021-12-08 21:01:24 +0500

    Practice mod 18.6

---
+++ Practice_mod 18_6/app.py
@@ -0,0 +1,44 @@
+import telebot
+from config import keys, TOKEH
+from extensions import ConvertionException, CryptoConverter
+
+
+bot = telebot.TeleBot(TOKEH)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    text = 'Чтобы начать работу введите команду боту в следующем формате, разделяя данные пробелом: \n \
+<имя валюты цену которой он хочет узнать> \
+<имя валюты в которой надо узнать цену первой валюты> \
+<количество первой валюты> \n\n \
+Увидеть список всех доступных валют: /values'
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key))
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text'])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+
+        if len(values) != 3:
+            raise ConvertionException('Слишком много параметров.')
+
+        base, quote, amount = values
+        total_base = CryptoConverter.get_price(base, quote, amount)
+    except ConvertionException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+    else:
+        text = f'Цена {amount} {base} в {quote} - {total_base}'
+        bot.send_message(message.chat.id, text)
+
+
+bot.polling()
---
+++ Practice_mod 18_6/config.py
@@ -0,0 +1,7 @@
+TOKEH = "5007604748:AAHCE9_X5r3p5o-1hKUqChg-nl8jFR6iTGw"
+
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB'
+}
